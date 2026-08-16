0579d1f15696adc8778f1ec199a0dd8cfd4e544d
Maxim Kuzmenko <cycymaks16@gmail.com>
2026-08-16 08:58:53 +0300

    Initial commit

---
+++ src/data/images.js
@@ -0,0 +1,5 @@
+export const images = {
+    logo: new URL('../assets/images/logo.png', import.meta.url).href,
+    heroBanner: new URL('../assets/images/banner.webp', import.meta.url).href,
+    witcherHorse: new URL('../assets/images/witcher-horse.webp', import.meta.url).href
+}
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import './scss/main.scss'
+import { images } from './data/images'
+
+document.querySelector('[data-js="logo"]').src = images.logo;
