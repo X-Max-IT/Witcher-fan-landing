ae2b8054fd4775b2a98ef3fc13d7c05038e6db71
Maxim Kuzmenko <cycymaks16@gmail.com>
2026-08-17 15:32:30 +0300

    +about

--- src/main.js
+++ src/main.js
@@ -11,6 +11,8 @@ document.querySelector('[data-js="twitter"]').src = images.iconTwitter;
 document.querySelector('[data-js="youtube"]').src = images.iconYoutube;
 document.querySelector('[data-js="facebook"]').src = images.iconFacebook;
 
+document.querySelector('[data-js="geralt-horse"]').src = images.witcherHorse;
+
 
 initBurger();
 initHeroVideo();
